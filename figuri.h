ace138a90d8245844901f5eaf1330d92e516ec9f
Buddha-naveka <162609305+Buddha-naveka@users.noreply.github.com>
2024-03-09 08:34:22 +0400

    Update figuri.h

--- figuri.h
+++ figuri.h
@@ -10,10 +10,10 @@ using std::string;
 struct Figura
 {
 	string name;
-	int gox;
-	int goy;
-	int size;
-	int spin;
+	double gox=0;
+	double goy=0;
+	double size=100;
+	double spin=0;
 };
 
 class vsefiguri
@@ -22,10 +22,13 @@ public:
 	void setCircleFigura();
 	void setBoxFigura();
 	void setSquareFigura();
-	void displayFigura();
-	Figura getCircleFigura();
-	Figura getBoxFigura();
-	Figura getSquareFigura();
+	void coutista(const Figura& vivod);
+	void coutistb(const Figura& vivod);
+	void coutistc(const Figura& vivod);
+	//void displayFigura();
+	Figura getCircleFigura() const { return circle; }
+	Figura getBoxFigura()const { return box; }
+	Figura getSquareFigura()const { return square; }
 
 private:
 	Figura circle;
