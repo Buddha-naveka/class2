e5531f79d139db7a0afa495c879e838dc4e43567
Buddha-naveka <162609305+Buddha-naveka@users.noreply.github.com>
2024-03-09 08:32:37 +0400

    Update class2.cpp

--- class2.cpp
+++ class2.cpp
@@ -1,4 +1,4 @@
-﻿
+
 
 #include <iostream>
 #include "figuri.h"
@@ -6,22 +6,33 @@
 int main()
 {
     vsefiguri final;
-    int number;
-    cout << "Select a shapes!\n";
-    cout << "1.Circle" << endl;
-    cout << "2.Box" << endl;
-    cout << "3.Square" << endl;
-    cin>> number;
-    switch(number)
+    int number =0;
+    while (number != 4)
     {
-    case 1:
-        final.setCircleFigura();
-        break;
-    case 2:
-        final.setBoxFigura();
-        break;
-    case 3:
-        final.setSquareFigura();
-        break;
+        cout << "Select a shapes!\n";
+        cout << "1.Circle" << endl;
+        cout << "2.Box" << endl;
+        cout << "3.Square" << endl;
+        cout << "4.End" << endl;
+
+        cin >> number;
+        switch (number)
+        {
+        case 1:
+            final.setCircleFigura();
+            final.coutista(final.getCircleFigura());
+            cout << endl;
+            break;
+        case 2:
+            final.setBoxFigura();
+            final.coutistb(final.getBoxFigura());
+            cout << endl;
+            break;
+        case 3:
+            final.setSquareFigura();
+            final.coutistc(final.getSquareFigura());
+            cout << endl;
+            break;
+        }
     }
 }
